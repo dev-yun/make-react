fb49e6901fe5ff5231538747daae85bb0b14f03d
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-08-01 20:36:53 +0900

    Add react.js에 html의 props를 처리하는 기능 추가
    html 객체의 속성을 처리하는 기능(setAttribute)을 추가
    
    완성된 vdom(html객체)를 container에 추가하는 함수 작성

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // 3단계
 // react파일(html객체를 DOM 객체로 변환하는 파일)과 개발자가 작성할 실제 코드를 담은 파일을 나눈다.
 
-import { createDOM } from "./react";
+import { createDOM, render } from "./react";
 
 // DOM으로 변환할 객체
 // html 태그 구조에는 대그 이름, 속성들, 자식 요소 3가지가 있다.
@@ -20,17 +20,23 @@ const vdom = {
             children: [
                 {
                     tag: 'li',
-                    props: {},
+                    props: {
+                        style : "color:red",
+                    },
                     children: ["첫 번째 아이템"]
                 },
                 {
                     tag: 'li',
-                    props: {},
+                    props: {
+                        style : "color:blue",
+                    },
                     children: ["두 번째 아이템"]
                 },
                 {
                     tag: 'li',
-                    props: {},
+                    props: {
+                        style : "color:green",
+                    },
                     children: ["세 번째 아이템"]
                 }
             ]
@@ -38,4 +44,6 @@ const vdom = {
     ],
 }
 
-document.querySelector('#root').appendChild(createDOM(vdom));
+const container = document.querySelector("#root");
+
+render(vdom, container)
--- src/react.js
+++ src/react.js
@@ -6,11 +6,17 @@ export function createDOM(node) {
 
     const element = document.createElement(node.tag);
 
-    
+    Object.entries(node.props)
+      .forEach(([name, value]) => element.setAttribute(name, value));    
 
     node.children
       .map(createDOM)
       .forEach(element.appendChild.bind(element));
       
     return element;
+}
+
+// container만 받아서 appendchild하는 것을 숨기는 함수
+export function render(vdom, container) {
+    container.appendChild(createDOM(vdom));
 }
