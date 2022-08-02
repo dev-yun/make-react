bfb98fa1304d45e845779d7e62fa961a05e314b1
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-08-02 16:41:05 +0900

    Update 간단한 virtual DOM 구현
    virtual DOM이 동작하는 방식 학습
    (이전 DOM과 변화한 DOM을 비교하는 것이 아니라 DOM으로 convert하기 이전에 객체 상태일때 비교)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-// 7단계 클래스 컴포넌트
-// 클래스 컴포넌트는 처음 실행될때 instance를 만들어서 내부에 context와 scope를 갖기 때문에 상태(state)를 갖을 수 있다.
-// 때문에 이미 만들어진 instance의 render를 계속해서 업데이트하면서 상태를 업데이트 할 수 있다. (이렇게 하면 context의 상태를 유지하거나 상태만 변경하여 사용할 수 있다.)
-// 이처럼 하나의 instance를 만들어 component가 삭제될 때까지 context의 상태를 유지하고 render함수를 호출하는 식으로 작동하는 것이 클래스 컴포넌트의 매커니즘이다.
-// React는 class component가 업데이트 될때, 데이터(props)를 받을때, 삭제될때를 모두 파악하고 그때에 필요한 "라이프 사이클 메서드"들을 제공한다.
+// 8단계 virtual DOM
+// DOM처리는 React에게 맡기고 DOM보다 훨씬 쉬운 구조물(JSX)로 UI를 만들고 개발할 수 있게한 환경이 virtual DOM이라 할 수 있다.
+// virtual DOM의 가장 큰 장점은 컴포넌트의 변경점이 있으면 전체를 재 랜더링을 하는 것이 아니라 그 부분만 재랜더링하므로써 생산성을 높힌다는 점이 있다.
+// 원리 : 현재 DOM과 새로 변경될 DOM을 변경하여 변화될 부분을 찾는 것은 매우 어렵다. 그래서 React는 DOM을 비교하는 것이 아니라 
+//        객체대 객체로 비교하는 방식으로 변경된 부분을 찾는다. *여기서 객체란 jsx로 작성되고 React.createElement로 변환된 객체를 의미한다.
 
-// 함수 컴포넌트는 호출할때마다 scope가 생성되어 호출되기 때문에 지속적으로 유지되는 상태(state)가 존재하지 않는다. (호출될 때마다 초기화된 상태로 불러오기 때문에)
-// 하지만 함수 컴포넌트에서도 Hook이 등장하면서 상태를 저장할 수 있게 되었고 함수컴포넌트를 사용하는 방식으로 변화하고 있다.
+// 이처럼 실제 변경된 부분을 객체를 통해 찾고 DOM에서 이 부분만 변경하여 랜더링하는 방식이 React의 최대 장점이다.
+
 
 /* @jsx makeElement */
 import { makeElement, render, Component } from "./react";
@@ -25,7 +25,7 @@ function Item(props) {
 
 
 const App = () => <p>
-    <Title>React 클래스 컴포넌트 잘 만들기</Title>
+    <Title>React 클래스 컴포넌트 잘 만들기(virtual DOM)</Title>
     <ul>
         <Item color="red">첫 번째 아이템</Item>
         <Item color="blue">두 번째 아이템</Item>
--- src/react.js
+++ src/react.js
@@ -1,15 +1,12 @@
 
-// 함수 컴포넌트에선 데이터(props)를 인자로 입력받지만
-// 클래스 컴포넌트에서는 데이터를 context로 입력받는다.
 export class Component {
     constructor(props) {
         this.props = props;
     }
 }
 
-
-// 만들어진 객체를 DOM으로 변환
-export function createDOM(node) {
+// makeElement를 통해 만들어진 객체를 통해 실제 DOM 트리를 만든다.
+function createDOM(node) {
     if(typeof node === 'string'){
         return document.createTextNode(node);
     }
@@ -33,28 +30,45 @@ function makeProps(props, children) {
     }
 }
 
+// makeElement()를 통해 만들어진 객체가 virtual DOM을 만들기 위한 메타 데이터가 된다.
 export function makeElement(tag, props, ...children) {
     props = props || {};
-    
+
     if (typeof tag === 'function'){
-        // 이 코드는 호출될때마다 새로운 instance 객체가 만들어지고 상태가 초기화 되기때문에 완벽한 클래스 컴포넌트는 아니다. 
-        // 처음 컴포넌트가 호출될때만 instance를 만들고 그 이후에는 만들어진 instacne context의 상태를 관리하는 라이프 사이클 메서드들이 필요하다.
         if (tag.prototype instanceof Component){
             const instance = new tag(makeProps(props, children))
             return instance.render();
-        } else {
-            if(children.length > 0){
-                return tag(makeProps(props, children))
-            }else{
-                return tag(props);
-            }
+        } 
+        
+        if(children.length > 0){
+            return tag(makeProps(props, children))
+        }else{
+            return tag(props);
         }
-    }else{
-        return { tag, props, children }
     }
+
+    // virtual DOM의 Input data 역할을 하게 됨
+    return { tag, props, children };
 }
 
-// container만 받아서 appendchild하는 것을 숨기는 함수
-export function render(vdom, container) {
-    container.appendChild(createDOM(vdom));
-}
+
+// 기존엔 vdom을 그대로 받아 DOM을 생성하는 방식이였는데
+// virtual DOM의 컨셉에 맞춰 객체를 비교하고 변화된 부분을 찾아 render하는 방식으로 변경함
+
+// render를 즉시 실행 함수로 만들어 반환 function 부분을 export하고 
+// prevDOM은 클로저에 갇혀 바깥쪽에서 접근할 수 없게 한다.
+export const render = (function() {
+    // 비교할 이전 DOM
+    let prevDom = null;
+
+    // 클로저를 사용하기 위한 함수 반환 구조
+    return function(vdom, container){
+        if(prevDom === null){
+            prevDom = vdom;
+        }
+
+        // 만약 prevDom이 null이 아니라 어떠한 값이 존재한다면
+        // 객체 수준에서 비교를 하고 변경사항 부분만 업데이트를 진행 => 업데이트를 위한 Update 함수를 구현하고 사용해야함. (create는 사용할 수 없음(가장 처음 DOM을 그릴때만 사용))
+        container.appendChild(createDOM(prevDom));
+    }
+})();
