bba73942d3db7e179a4eb7bc732c63e64386a7af
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-08-02 17:41:06 +0900

    Update React Hook의 원리 학습
    Hook을 사용할때 규칙을 알아보고
    
    Hook의 동작 원리를 살펴보며 왜 이런 규칙이 생겼는지 학습하고
    
    함수 컴포넌트에서 어떻게 상태를 저장할 수 있게 됐는지 알아보았다.

--- src/react.js
+++ src/react.js
@@ -1,3 +1,8 @@
+// 함수 컴포넌트는 상태를 저장할 수 없는데 외부에 상태를 저장할 값(hooks)를 만들고 상태를 index 순서에 맞춰 저장함으로 써 함수컴포넌트에서도 상태를 저장할 수 있게 된다.
+// 이때 상태를 저장하는 hooks는 배열이고 index 값에 맞춰 실행되기 때문에 순서가 매우 중요하고 이 덕분에 순서를 보장받을 수 있다.
+const hooks = [];
+let currentComponent = 0;
+
 
 export class Component {
     constructor(props) {
@@ -30,6 +35,23 @@ function makeProps(props, children) {
     }
 }
 
+function useState(initValue) {
+    let curPosition = currentComponent - 1;
+
+    // 현재 component의 값이 비었을때 초기값 설정
+    if(!hooks[curPosition]){
+        hooks[curPosition] = initValue;  
+    }
+
+    // 변한값(nextValue)를 현재 component에 추가하는 함수
+    const modifier = nextValue => {
+        hooks[curPosition] = nextValue;
+    };
+
+    // 값과 
+    return [ hooks[curPosition], modifier ]
+}
+
 // makeElement()를 통해 만들어진 객체가 virtual DOM을 만들기 위한 메타 데이터가 된다.
 export function makeElement(tag, props, ...children) {
     props = props || {};
@@ -40,6 +62,10 @@ export function makeElement(tag, props, ...children) {
             return instance.render();
         } 
         
+        // makeElement가 실행될때마다 component가 생성되고 componenent가 생성되면 해당 index에 hook의 기능을 넣어 매핑한다. (순서 보장을 위해서)
+        hooks[currentComponent] = null;
+        currentComponent++;
+
         if(children.length > 0){
             return tag(makeProps(props, children))
         }else{
